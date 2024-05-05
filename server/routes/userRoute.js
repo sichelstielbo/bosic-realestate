1faecadc4b540566715bb58f6184866f826a534b
sichelstielbo <129999134+sichelstielbo@users.noreply.github.com>
2024-05-05 14:38:49 -0400

    5/5 changes

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,12 +1,13 @@
 import express from "express";
+import jwtCheck from "../config/auth0Config.js";
 import { bookVisit, cancelBooking, createUser, getAllBookings, getAllFavorites, toFav } from "../controllers/userCntrl.js";
 const router = express.Router()
 
-router.post("/register", createUser);
+router.post("/register", jwtCheck, createUser);
 //Created a post request to the endpoint of "register", when the endpoint is reached, the createUser function will execute
-router.post("/bookVisit/:id", bookVisit)
+router.post("/bookVisit/:id", jwtCheck, bookVisit)
 router.post("/allBookings", getAllBookings)
-router.post("/removeBooking/:id", cancelBooking)
-router.post("/toFav/:rid", toFav)
-router.post("/allFav/", getAllFavorites)
+router.post("/removeBooking/:id", jwtCheck, cancelBooking)
+router.post("/toFav/:rid", jwtCheck, toFav)
+router.post("/allFav/", jwtCheck, getAllFavorites)
 export { router as userRoute }
